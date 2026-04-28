f2623adbbd0e799ae5991e45363595c55a185e0a
JosiahKou <155879792+JosiahKou@users.noreply.github.com>
2026-04-28 16:42:35 +0000

    done?

--- src/main.c
+++ src/main.c
@@ -1,34 +1,23 @@
-/************* t.c file of C2.5 **************/
-int v[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};  // data array
-int sum;
+#include "uart.h"
+#include <stdint.h>
 
-#include <string.h>  // contains strlen(), strcmp(), etc.
-
-#include "uart.h"  // UART driver code file
-
-int main() {
+int cs122a_main() {
   int i;
-  sum = 10;
-  char string[64];
   UART* up;
-  uart_init();  // initialize UARTs
-  sum = 100;
-  up = &uart[0];  // test UART0
-  uprints(up, "Enter lines from serial terminal 0\n\r");
-  sum = 1000;
-  while (1) {
-    ugets(up, string);
-    uprints(up, " ");
-    uprints(up, string);
-    uprints(up, "\n\r");
-    if (strcmp(string, "end") == 0) break;
+  
+  uart_init();
+  
+  // Use only UART[0] for output (it's connected to the console)
+  up = &uart[0];
+  
+  for (i = 0; i < 4; i++) {
+    uint32_t divisor = *(uint32_t*)(uart[i].base + UARTIBRD);
+    uint32_t baudrate = 7380000 / (16 * divisor);
+    uint32_t lcr = *(uint32_t*)(uart[i].base + UARTLCR);
+    
+    uprintf(up, "UART[%d] Baudrate: %d\n\r", i, baudrate);
+    uprintf(up, "UART[%d] Line Control Register: 0x%x\n\r", i, lcr);
   }
-
-  uprints(up, "Compute sum of array:\n\r");
-  sum = 0;
-  for (i = 0; i < 10; i++) sum += v[i];
-  uprints(up, "sum = ");
-  uputc(up, (sum / 10) + '0');
-  uputc(up, (sum % 10) + '0');
-  uprints(up, "\n\rEND OF RUN\n\r");
+  
+  return 0;
 }
--- src/uart.c
+++ src/uart.c
@@ -1,82 +1,130 @@
 #include "uart.h"
-
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-/******** uart.c file of C2.5 : UART Driver Code ********/
+UART uart[4];
 
-UART uart[4];  // 4 UART structures
+// Helper function for integer to string conversion (renamed to avoid conflict)
+static char* int_to_str(int val, char* str, int base) {
+  static char* digits = "0123456789abcdef";
+  char* ptr = str;
+  int num = val;
+  int is_negative = 0;
+  
+  if (base == 10 && val < 0) {
+    is_negative = 1;
+    num = -val;
+  }
+  
+  if (num == 0) {
+    *ptr++ = '0';
+    *ptr = '\0';
+    return str;
+  }
+  
+  char* start = ptr;
+  while (num > 0) {
+    *ptr++ = digits[num % base];
+    num /= base;
+  }
+  
+  if (is_negative) {
+    *ptr++ = '-';
+  }
+  
+  *ptr = '\0';
+  
+  char* end = ptr - 1;
+  while (start < end) {
+    char temp = *start;
+    *start++ = *end;
+    *end-- = temp;
+  }
+  
+  return str;
+}
 
-uint8_t uart_rate_divisors[] = {0x04, 0x0C, 0x18, 0x20, 0x30};
-
-// UART initialization function
 int uart_init() {
   int i;
   UART* up;
-  for (i = 0; i < 4; i++) {  // uart0 to uart2 are adjacent
+  
+  // Set base addresses
+  for (i = 0; i < 3; i++) {
     up = &uart[i];
     up->base = (char*)(0x101F1000 + i * 0x1000);
     up->n = i;
   }
-  uart[3].base = (char*)(0x10009000);  // uart3 at 0x10009000
+  uart[3].base = (char*)(0x10009000);
+  uart[3].n = 3;
+  
+  // Configure each UART
+  uint32_t divisors[] = {0x30, 0x20, 0x18, 0x0C};
+  
+  for (i = 0; i < 4; i++) {
+    up = &uart[i];
+    *(uint32_t*)(up->base + UARTIBRD) = divisors[i];
+    *(uint32_t*)(up->base + UARTLCR) = 0x60;
+  }
+  
+  return 0;
 }
 
-// input a char from UART pointed by up
 int ugetc(UART* up) {
-  while (*(up->base + UARTFR) & RXFE);  // loop if UFR is RXFE
-  return *(up->base + UARTDR);          // return a char in UDR
+  while (*(uint32_t*)(up->base + UARTFR) & 0x10);
+  return *(uint32_t*)(up->base + UARTDR);
 }
 
-// output a char to UART pointed by up
 int uputc(UART* up, char c) {
-  while (*(up->base + UARTFR) & TXFF);  // loop if UFR is TXFF
-  *(up->base + UARTDR) = c;             // write char to data register
+  while (*(uint32_t*)(up->base + UARTFR) & 0x20);
+  *(uint32_t*)(up->base + UARTDR) = c;
+  return 0;
 }
 
-// input a string of chars
 int ugets(UART* up, char* s) {
   while ((*s = ugetc(up)) != '\r') {
     uputc(up, *s);
     s++;
   }
   *s = 0;
+  return 0;
 }
 
-// output a string of chars
 int uprints(UART* up, char* s) {
   while (*s) uputc(up, *s++);
+  return 0;
 }
 
-#define MAX_STRING_LEN 255
 int uprintu(UART* up, uint32_t val) {
-  char output[MAX_STRING_LEN];
-  uprints(up, itoa(val, output, 10));
+  char output[32];
+  uprints(up, int_to_str(val, output, 10));
+  return 0;
 }
 
 int uprintd(UART* up, int val) {
-  char output[MAX_STRING_LEN];
-  uprints(up, itoa(val, output, 10));
+  char output[32];
+  uprints(up, int_to_str(val, output, 10));
+  return 0;
 }
 
 int uprintx(UART* up, uint32_t val) {
-  char output[MAX_STRING_LEN];
-  uprints(up, itoa(val, output, 16));
+  char output[32];
+  uprints(up, int_to_str(val, output, 16));
+  return 0;
 }
 
 int uprintf(UART* up, char* fmt, ...) {
-  char* cp = fmt;            // cp points to the fmt string
-  int* ip = (int*)&fmt + 1;  // ip points to first item in stack
-  while (*cp) {              // scan the format string
-    if (*cp != '%') {        // spit out ordinary chars
+  char* cp = fmt;
+  int* ip = (int*)&fmt + 1;
+  
+  while (*cp) {
+    if (*cp != '%') {
       uputc(up, *cp);
-      if (*cp == '\n')    // for each ‘\n’
-        uputc(up, '\r');  // print a ‘\r’
+      if (*cp == '\n')
+        uputc(up, '\r');
       cp++;
       continue;
     }
-    cp++;           // cp points at a conversion symbol
-    switch (*cp) {  // print item by %FORMAT symbol
+    cp++;
+    switch (*cp) {
       case 'c':
         uputc(up, (char)*ip);
         break;
@@ -94,6 +142,7 @@ int uprintf(UART* up, char* fmt, ...) {
         break;
     }
     cp++;
-    ip++;  // advance pointers
+    ip++;
   }
+  return 0;
 }
